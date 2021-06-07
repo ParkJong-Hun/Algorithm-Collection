cf1e687eebbdee2094eb9adbaf6250a1693d3ffd
ParkJong-Hun <thesn515@gmail.com>
2021-06-07 16:30:31 +0900

    2021/06/07

---
+++ prime-number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void findPrimeNumber(int a) {
+    int i, j;
+    i = a - 1;
+    j = 2;
+    while(1) {
+        if (j <= 1)
+            if(a % j == 0) {
+                cout << "소수 아님";
+                break;
+            } else j++;
+        else {
+            cout << "소수";
+            break;
+        }
+    }
+}
+
+int main(void) {
+    findPrimeNumber(3);
+    return 0;
+}
